e3589d85e7f370efed9ce1959345c36339fa59e5
mojo211922 <86511374+mojo211922@users.noreply.github.com>
2021-08-07 21:08:54 +0800

    跳+跑

--- 2d game/Assets/jk53.cs
+++ 2d game/Assets/jk53.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class jk53 : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator anim;
+    public Collider2D coll;
     public float speed;
     public float jumpforce;
+    public LayerMask ground;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,25 +19,45 @@ public class jk53 : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Movement();
+        SwitchAnim();
     }
 
     void Movement()
     {
+        //角色移動
         float horizontalmove = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(horizontalmove * speed, rb.velocity.y);
+        anim.SetFloat("走路開關0", Mathf.Abs(horizontalmove));
         if (horizontalmove != 0) //角色轉身 
         {
             transform.localScale = new Vector3(horizontalmove, 1, 1);
         }
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))//腳色跳
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+            anim.SetBool("跳開關", true);
         }
+    }
 
-    
-    
+    void SwitchAnim()
+    {
+        anim.SetBool("待機開關", false);
+
+        if (anim.GetBool("跳開關"))
+        {
+            if (rb.velocity.y < 0)
+            {
+                anim.SetBool("跳開關", false);
+                anim.SetBool("落下開關", true);
+            }
+        }else if (coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("落下開關", false);
+            anim.SetBool("待機開關", true);
+        }
+        
     }
 }

6892ad70a21a1e778b20cfc4544823ef66bb2f03
mojo211922 <86511374+mojo211922@users.noreply.github.com>
2021-10-09 19:02:59 +0800

    bt

---
+++ 元素騎士/Assets/Scenes/jk53.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jk53 : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator anim;
+    public Collider2D coll;
+    public float speed;
+    public float jumpforce;
+    public LayerMask ground;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+        SwitchAnim();
+        Attack();
+    }
+
+    void Movement()
+    {
+        //角色移動
+        float horizontalmove = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalmove * speed, rb.velocity.y);
+        anim.SetFloat("走路開關0", Mathf.Abs(horizontalmove));
+        if (horizontalmove != 0) //角色轉身 
+        {
+            transform.localScale = new Vector3(horizontalmove, 1, 1);
+        }
+        if (Input.GetButtonDown("Jump"))//腳色跳
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+            anim.SetBool("跳開關", true);
+        }
+    }
+
+    void Attack()
+    {
+        if (Input.GetButtonDown("Attack"))
+        {
+            anim.SetTrigger("攻擊觸發");
+        }
+    }
+    void SwitchAnim()
+    {
+        anim.SetBool("待機開關", false);
+
+        if (anim.GetBool("跳開關"))
+        {
+            if (rb.velocity.y < 0)
+            {
+                anim.SetBool("跳開關", false);
+                anim.SetBool("落下開關", true);
+            }
+        }else if (coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("落下開關", false);
+            anim.SetBool("待機開關", true);
+        }
+        
+    }
+}
